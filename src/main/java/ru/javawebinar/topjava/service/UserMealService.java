7b73f350c736459f98cc4c0081224af0c586892c
Shakirov <d31m07y1988@gmail.com>
2016-06-12 16:08:29 +0300

    сделаны сервисы для meals

--- src/main/java/ru/javawebinar/topjava/service/UserMealService.java
+++ src/main/java/ru/javawebinar/topjava/service/UserMealService.java
@@ -1,8 +1,24 @@
 package ru.javawebinar.topjava.service;
 
+import ru.javawebinar.topjava.model.UserMeal;
+import ru.javawebinar.topjava.util.exception.NotFoundException;
+
+import java.util.Collection;
+import java.util.List;
+
 /**
  * GKislin
  * 15.06.2015.
  */
 public interface UserMealService {
+
+    UserMeal save(Integer userId, UserMeal userMeal) throws NotFoundException;
+
+    void delete(Integer userId, int id);
+
+    UserMeal get(Integer userId, int id) throws NotFoundException;
+
+    Collection<UserMeal> getAll(Integer userId);
+
+    void update(Integer userId, UserMeal userMeal);
 }
--- src/main/java/ru/javawebinar/topjava/service/UserMealServiceImpl.java
+++ src/main/java/ru/javawebinar/topjava/service/UserMealServiceImpl.java
@@ -1,13 +1,46 @@
 package ru.javawebinar.topjava.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import ru.javawebinar.topjava.model.UserMeal;
 import ru.javawebinar.topjava.repository.UserMealRepository;
+import ru.javawebinar.topjava.util.exception.ExceptionUtil;
+import ru.javawebinar.topjava.util.exception.NotFoundException;
+
+import java.util.Collection;
 
 /**
  * GKislin
  * 06.03.2015.
  */
+@Service
 public class UserMealServiceImpl implements UserMealService {
 
+    @Autowired
     private UserMealRepository repository;
 
+    @Override
+    public UserMeal save(Integer userId, UserMeal userMeal) {
+        return ExceptionUtil.checkNotFound(repository.save(userId, userMeal), "meal: " + userMeal.getId()+" for user:" + userId);
+    }
+
+    @Override
+    public void delete(Integer userId, int id) {
+        repository.delete(userId, id);
+    }
+
+    @Override
+    public UserMeal get(Integer userId, int id) throws NotFoundException {
+        return ExceptionUtil.checkNotFoundWithId(repository.get(userId, id), id);
+    }
+
+    @Override
+    public Collection<UserMeal> getAll(Integer userId) {
+        return repository.getAll(userId);
+    }
+
+    @Override
+    public void update(Integer userId, UserMeal userMeal) {
+        repository.save(userId, userMeal);
+    }
 }
