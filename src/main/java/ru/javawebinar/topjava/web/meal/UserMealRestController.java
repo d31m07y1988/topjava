836e3a9e9497043888e260fe28605888acb2c851
Shakirov <d31m07y1988@gmail.com>
2016-06-12 16:56:15 +0300

    Контроллер блюд

--- src/main/java/ru/javawebinar/topjava/web/meal/UserMealRestController.java
+++ src/main/java/ru/javawebinar/topjava/web/meal/UserMealRestController.java
@@ -1,12 +1,61 @@
 package ru.javawebinar.topjava.web.meal;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import ru.javawebinar.topjava.LoggedUser;
+import ru.javawebinar.topjava.model.User;
+import ru.javawebinar.topjava.model.UserMeal;
 import ru.javawebinar.topjava.service.UserMealService;
+import ru.javawebinar.topjava.service.UserService;
+
+import java.util.Collection;
 
 /**
  * GKislin
  * 06.03.2015.
  */
+@Controller
 public class UserMealRestController {
+    protected final Logger LOG = LoggerFactory.getLogger(getClass());
+
+/*    @Autowired
+    private UserService userService;
+
+    public User get() {
+        return userService.get(LoggedUser.id());
+    }
+
+    private LoggedUser loggedUser = new LoggedUser();*/
+
+    @Autowired
     private UserMealService service;
 
+    public Collection<UserMeal> getAll() {
+        LOG.info("getAll");
+        return service.getAll(LoggedUser.id());
+    }
+
+    public UserMeal get(int id) {
+        LOG.info("get " + id);
+        return service.get(LoggedUser.id(), id);
+    }
+
+    public UserMeal create(UserMeal userMeal) {
+        userMeal.setId(null);
+        LOG.info("create " + userMeal);
+        return service.save(LoggedUser.id(), userMeal);
+    }
+
+    public void delete(int id) {
+        LOG.info("delete " + id);
+        service.delete(LoggedUser.id(), id);
+    }
+
+    public void update(UserMeal userMeal, int id) {
+        userMeal.setId(id);
+        LOG.info("update " + userMeal);
+        service.update(LoggedUser.id(), userMeal);
+    }
 }
